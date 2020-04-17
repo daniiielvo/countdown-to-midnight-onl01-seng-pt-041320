f09c88ea85b780ee2f88831c9a97aeb64a35a477
Daniel Vo <daniiielvo@gmail.com>
2020-04-17 00:33:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
-def countdown_with_sleep (number)
-  number = 12 
+def countdown (number)
+  number = 10  
   while number > 0 
   number -= 1
   puts "#{number} SECOND(S)!"
-  sleep 1
+  
   
   return "HAPPY NEW YEAR"
 end 
